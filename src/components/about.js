967569d32d1eca7c9ad6a75ff74ec7e695abb9ae
HomuraShu <esy.minmax@gmail.com>
2023-04-10 08:08:44 +0800

    yeyey

---
+++ src/components/about.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+function about(){
+  return (
+    <>
+        <h1>IT WORKED ABOUT</h1>
+        <h1>IT WORKED ABOUT</h1>
+        <h1>IT WORKED ABOUT</h1>
+        <h1>IT WORKED ABOUT</h1>
+    </>
+  )
+}
+
+export default about
